afa58f9a5ce2cb717ecb4e9ded923b2e80aa7110
Martua Nasution <marithonst@gmail.com>
2021-12-24 04:46:09 +0700

    add vercel auto deploy

--- test/Homepage.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Homepage from '@/pages/index.vue'
-
-describe('pages/index.vue', () => {
-  test('is a Homepage', () => {
-    const wrapper = bro(Homepage)
-    expect(wrapper.vm).toContain("Cari gambar...")
-  })
-})
